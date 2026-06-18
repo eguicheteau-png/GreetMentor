580aaa4d41d68d2101a5b4960526cf592f581c75
Warren Keumeugni <wkeumeumeugni@edenschool.fr>
2026-06-18 11:11:27 +0200

    added homepage css

--- views/accueil.php
+++ views/accueil.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main.css">
     <title>GreetMentor</title>
 </head>
 <body>
@@ -20,7 +21,7 @@
 
             <div>
                 <a href="/login">Log In</a>
-                <a href="#">Sign Up</a>
+                <a href="/account/create/form">Sign Up</a>
             </div>
         </nav>
     </header>
